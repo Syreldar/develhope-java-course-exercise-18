e819ae18a1722c9a4f319ecda3735578b7abf00c
Syreldar <enrico.drago3@gmail.com>
2023-07-11 15:17:30 +0200

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,3 +1,28 @@
 public class Employee
 {
+    private final String name;
+    private final String surname;
+    private final String address;
+
+    public Employee(String newEmployeeName, String newEmployeeSurname, String newEmployeeAddress)
+    {
+        this.name = newEmployeeName;
+        this.surname = newEmployeeSurname;
+        this.address = newEmployeeAddress;
+    }
+
+    public String getEmployeeDetails()
+    {
+        return String.format("\nName: %s\nSurname: %s\nAddress: %s", name, surname, address);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getSurname()
+    {
+        return surname;
+    }
 }
