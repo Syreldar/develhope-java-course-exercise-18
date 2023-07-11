96994e5b0d70f55b9a64abde07eac682bb82e2d9
Syreldar <enrico.drago3@gmail.com>
2023-07-11 15:19:41 +0200

    Upload exercise

--- Tester.java
+++ Tester.java
@@ -3,5 +3,16 @@ public class Tester
 {
     public static void main(String[] args)
     {
+        Employee emp1 = new Employee("Enrico", "Drago", "Viale dei Tigli, 16");
+        Badge badge1 = new Badge(emp1);
+
+        System.out.println("Badge 1 Details:");
+        badge1.showBadgeDetails();
+
+        Employee emp2 = new Employee("Rossella", "Ferrau", "Via Noviziato Casazza, 3");
+        Badge badge2 = new Badge(emp2);
+
+        System.out.println("\nBadge 2 Details:");
+        badge2.showBadgeDetails();
     }
 }
