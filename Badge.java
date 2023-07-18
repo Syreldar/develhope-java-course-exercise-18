e20dc1b50b43015a9bcc2dbade804309c3b1bed9
Syreldar <enrico.drago3@gmail.com>
2023-07-18 09:44:48 +0200

    Split in two methods.

--- Badge.java
+++ Badge.java
@@ -2,6 +2,8 @@ import java.util.Random;
 
 public class Badge
 {
+    private static final int CODE_LENGTH = 3;
+
     private static int totalNumberOfEmployees = 0;
     private final String badgeIdCode;
     public Employee employee;
@@ -20,18 +22,25 @@ public class Badge
 
     private String generateBadgeIdCode()
     {
-        int MIN_NUMBER = 100;
-        int MAX_NUMBER = 999;
-        Random rand = new Random();
+        String randomCode1 = generateRandomCode();
+        String randomCode2 = generateRandomCode();
 
-        String randomCode1 = String.valueOf(rand.nextInt(MIN_NUMBER, MAX_NUMBER+1));
-        String randomCode2 = String.valueOf(rand.nextInt(MIN_NUMBER, MAX_NUMBER+1)); // Same as above.
         return String.format("%s%s%s%s",
                 randomCode1,
                 employee.getName(),
                 employee.getSurname(),
                 randomCode2
         );
+    }
+
+    private String generateRandomCode()
+    {
+        int MIN_NUMBER = 100;
+        int MAX_NUMBER = 999;
+        Random rand = new Random();
+
+        int randomNumber = rand.nextInt((MAX_NUMBER - MIN_NUMBER) + 1) + MIN_NUMBER;
+        return String.valueOf(randomNumber);
     }
 
     public void showBadgeDetails()
